7814cb0461e009b7b0e7245ef804a6f1db90a9a0
Mantrakaar <mantrakaar@mantrakaar.com>
2026-08-04 23:38:26 +0530

    chore: remove unused raw files, demo folders, and build/scrape scripts

--- extract.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const { JSDOM } = require('jsdom');
-const clientHtml = fs.readFileSync('.system_generated/steps/1262/content.md', 'utf8');
-const testHtml = fs.readFileSync('.system_generated/steps/1263/content.md', 'utf8');
-const clientDom = new JSDOM(clientHtml);
-const testDom = new JSDOM(testHtml);
-fs.writeFileSync('raw_clients.html', clientDom.window.document.documentElement.outerHTML);
-fs.writeFileSync('raw_testimonials.html', testDom.window.document.documentElement.outerHTML);
